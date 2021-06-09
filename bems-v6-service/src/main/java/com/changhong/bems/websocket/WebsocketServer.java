8055517b6d91b25280d6d9892372728277ed181e
Vision.Mac <chao2.ma@changhong.com>
2021-06-09 18:23:23 +0800

    预算导入

--- bems-v6-service/src/main/java/com/changhong/bems/websocket/WebsocketServer.java
+++ bems-v6-service/src/main/java/com/changhong/bems/websocket/WebsocketServer.java
@@ -67,13 +67,12 @@ public class WebsocketServer {
             }
             statistics = new OrderStatistics();
             send(session, ResultData.success(statistics));
+            // 更新订单是否正在异步处理行项数据.如果是,在编辑时进入socket状态显示页面
+            orderService.setProcessStatus(orderId, Boolean.FALSE);
         } catch (Exception e) {
             LOG.error("websocket获取预算申请单处理日志异常:" + ExceptionUtils.getRootCauseMessage(e), e);
             // 输出最新日志
             send(session, ResultData.fail("websocket获取预算申请单处理日志异常:" + ExceptionUtils.getRootCauseMessage(e)));
-        } finally {
-            // 更新订单是否正在异步处理行项数据.如果是,在编辑时进入socket状态显示页面
-            orderService.setProcessStatus(orderId, Boolean.FALSE);
         }
     }
 
